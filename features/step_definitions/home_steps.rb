a82ef28a43cf2e0a4e8eb76c41ccd71d8d9aef0e
roofimon <roofimon@gmail.com>
2013-04-09 15:30:43 +0900

    Finish cucumber specs for contact, about, overview and anime

--- features/step_definitions/home_steps.rb
+++ features/step_definitions/home_steps.rb
@@ -13,3 +13,21 @@ end
 Then(/^Page should contains "(.*?)"$/) do |image|
   page.should have_xpath("//img[@src=\"#{image}\"]")
 end
+
+Given(/^I am on the anime page$/) do
+  visit '/#anime'
+end
+
+Then(/^Page should contains a beautiful "(.*?)"$/) do |anime_image|
+  page.should have_xpath("//img[@src=\"#{anime_image}\"]")
+end
+
+Given(/^I am on the overview page$/) do
+  visit '/#overview'
+end
+
+Then(/^Page should contains a good "(.*?)"$/) do |overview_image|
+  page.should have_xpath("//img[@src=\"#{overview_image}\"]")
+end
+
+
