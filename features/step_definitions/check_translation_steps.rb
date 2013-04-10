4dcc4fd70837c4dc829b6a86f8f7de536941def6
roofimon <roofimon@gmail.com>
2013-04-10 17:06:48 +0900

    Refactor cucumber feature

--- features/step_definitions/check_translation_steps.rb
+++ features/step_definitions/check_translation_steps.rb
@@ -2,19 +2,13 @@ Given(/^I am on the home page$/) do
   visit '/'
 end
 
-When(/^I click on '\/translation' link$/) do
+When(/^I go to translation page$/) do
   click_link 'Translations'
-  page.status_code.should == 200
 end
 
-Then(/^It should show me "(.*?)"$/) do |simplify_chinese|
-   page.should have_image simplify_chinese 
+Then(/^I will see scrum primer in simplified chinese$/) do
+  page.should have_image '/overview/scrum_overview_simplified_chinese.png' 
+  page.should have_link( "Scrum Overview Version 1", {:href =>"/overview/scrum_overview1_simplified_chinese.pdf" })
+  page.should have_link( "Scrum Overview Version 2", {:href =>"/overview/scrum_overview2_simplified_chinese.pdf" })
 end
 
-Then(/^I should be able to download "(.*?)" from "(.*?)"$/) do |pdf_simplify_chinese_v1, url_pdf_simplify_chinese_v1|  
-  page.should have_link( url_pdf_simplify_chinese_v1, {:href => pdf_simplify_chinese_v1})
-end
-
-Then(/^I also should be able to download "(.*?)" from "(.*?)"$/) do |pdf_simplify_chinese_v2, url_pdf_simplify_chinese_v2|
-  page.should have_link( url_pdf_simplify_chinese_v2, {:href => pdf_simplify_chinese_v2})
-end
