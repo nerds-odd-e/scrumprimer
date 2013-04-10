ee241348f831eddf24b29c241b21753ba7a8105d
Michael Mallete <mrmallete@gmail.com>
2013-04-10 16:36:32 +0900

    added translations to home and translations page

--- spec/international_scrumprimer_spec.rb
+++ spec/international_scrumprimer_spec.rb
@@ -27,10 +27,24 @@ describe "International Scrum Primer" do
   end	
 
   it "has tabs that will use locale when accessed initially with that specific locale" do
-  	visit '/cn/home'
+  	visit '/cn'
   	click_link '翻译'
   	current_path.should== '/cn/translations'  
   end	
 
+  it "has English translation on the homepage" do
+    visit '/'
+    page.should have_content "Download here - Print quality"
+  end
+  
+  it "has Chinese translation on the homepage" do
+    visit '/cn'
+    page.should have_content "下载浏览质量文件"
+  end 
+
+  it "has Chinese translation on the translations page" do
+    visit '/cn/translations'
+    page.should have_content "Scrum 简章"
+  end  
 
 end
