c73b2ac7cbbee4c6388a3db9cd7144bb3ef77d5b
roofimon <roofimon@gmail.com>
2013-04-10 17:45:22 +0900

    Refactor cucumber check translation steps

--- features/step_definitions/check_translation_steps.rb
+++ features/step_definitions/check_translation_steps.rb
@@ -7,8 +7,16 @@ When(/^I go to translation page$/) do
 end
 
 Then(/^I will see scrum primer in (.*)$/) do |language|
-  page.should have_image '/overview/scrum_overview_'+language+'.png' 
-  page.should have_link( "Scrum Overview Version 1", {:href =>"/overview/scrum_overview1_"+language+".pdf" })
-  page.should have_link( "Scrum Overview Version 2", {:href =>"/overview/scrum_overview2_"+language+".pdf" })
+  confirm_scrum_primer_in language
+  confirm_download_link language, "1"
+  confirm_download_link language, "2"
+end
+
+def confirm_scrum_primer_in(language)
+  page.should have_image '/overview/scrum_overview_'+language+'.png'
+end
+
+def confirm_download_link(language, version)
+  page.should have_link( "Scrum Overview Version "+version, {:href =>"/overview/scrum_overview"+version+"_"+language+".pdf" })
 end
 
