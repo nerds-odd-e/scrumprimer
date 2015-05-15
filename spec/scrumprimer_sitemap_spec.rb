503d475dde41052bcba1634ddecaa4e9764bfff3
Bas Vodde <basv@odd-e.com>
2015-05-15 12:46:16 +0800

    Changed to new rspec style

--- spec/scrumprimer_sitemap_spec.rb
+++ spec/scrumprimer_sitemap_spec.rb
@@ -15,36 +15,36 @@ describe "Scrum Primer Sitemap Specs" do
 
   it 'should have a sitemap.xml file' do
     visit '/sitemap.xml'
-    page.status_code.should == 200
+    expect(page.status_code).to eq 200
   end
 
   it 'should have a sitemap.xml entry for /' do
     visit '/sitemap.xml'
-    page.should have_content 'http://scrumprimer.org/'
+    expect(page).to have_content 'http://scrumprimer.org/'
   end
 
   it 'should have a sitemap.xml entry for /translations' do
     visit '/sitemap.xml'
-    page.should have_content 'http://scrumprimer.org/translations'
+    expect(page).to have_content 'http://scrumprimer.org/translations'
   end
 
   it 'should have a sitemap.xml entry for /overview' do
     visit '/sitemap.xml'
-    page.should have_content 'http://scrumprimer.org/overview'
+    expect(page).to have_content 'http://scrumprimer.org/overview'
   end
 
   it 'should have a sitemap.xml entry for /anime' do
     visit '/sitemap.xml'
-    page.should have_content 'http://scrumprimer.org/anime'
+    expect(page).to have_content 'http://scrumprimer.org/anime'
   end
 
   it 'should have a sitemap.xml entry for /about' do
     visit '/sitemap.xml'
-    page.should have_content 'http://scrumprimer.org/about'
+    expect(page).to have_content 'http://scrumprimer.org/about'
   end
 
   it 'should have a sitemap.xml entry for /contact' do
     visit '/sitemap.xml'
-    page.should have_content 'http://scrumprimer.org/contact'
+    expect(page).to have_content 'http://scrumprimer.org/contact'
   end
 end
