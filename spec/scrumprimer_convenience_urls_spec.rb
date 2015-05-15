7a80e85190cd172c7330f05ec55b348ee319e55c
Bas Vodde <basv@odd-e.com>
2015-05-15 12:45:43 +0800

    Changed to new rspec style

--- spec/scrumprimer_convenience_urls_spec.rb
+++ spec/scrumprimer_convenience_urls_spec.rb
@@ -16,37 +16,37 @@ describe "Convenience and backwards compatability URLS" do
 
   it "Can get the English file from the home directory" do
     visit '/scrumprimer20.pdf'
-    page.status_code.should == 200
-    page.response_headers['Content-Type'].should== "application/pdf"
+    expect(page.status_code).to eq 200
+    expect(page.response_headers['Content-Type']).to eq "application/pdf"
   end
-  
+
   it "Can redirect also the small version" do
     visit '/scrumprimer20_small.pdf'
-    page.status_code.should == 200
-    page.response_headers['Content-Type'].should== "application/pdf"
+    expect(page.status_code).to eq 200
+    expect(page.response_headers['Content-Type']).to eq "application/pdf"
   end
-  
+
   it "Can get the old English version, though it redirects to the new version" do
     visit '/scrumprimer120.pdf'
-    page.status_code.should == 200
-    page.response_headers['Content-Type'].should== "application/pdf"    
+    expect(page.status_code).to eq 200
+    expect(page.response_headers['Content-Type']).to eq "application/pdf"
   end
 
   it "Can get the beta 2.0 version, though it also redirects to the new version" do
     visit '/scrumprimer199.pdf'
-    page.status_code.should == 200
-    page.response_headers['Content-Type'].should== "application/pdf"    
+    expect(page.status_code).to eq 200
+    expect(page.response_headers['Content-Type']).to eq "application/pdf"
   end
-  
+
   it "Can redirect the Chinese version also the current version" do
     visit '/scrum_primer_cn.pdf'
-    page.status_code.should == 200
-    page.response_headers['Content-Type'].should== "application/pdf"    
+    expect(page.status_code).to eq 200
+    expect(page.response_headers['Content-Type']).to eq "application/pdf"
   end
 
   def link_should_work link
     visit link
-    page.status_code.should == 200
+    expect(page.status_code).to eq 200
   end
 
   # For backward comparability!
@@ -55,5 +55,5 @@ describe "Convenience and backwards compatability URLS" do
     link_should_work '/primers/scrumprimer20_simplified_chinese.pdf'
     link_should_work '/primers/scrumprimer20_bulgarian.pdf'
   end
-  
 end
+
